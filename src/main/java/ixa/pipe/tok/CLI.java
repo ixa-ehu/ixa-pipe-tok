c1dfe1c052ecddd11e8df0b18648e52fa5afbd0c
Rodrigo Agerri <agerri.rodrigo@gmail.com>
2013-12-16 18:20:34 +0100

    input kafraw added

--- src/main/java/ixa/pipe/tok/CLI.java
+++ src/main/java/ixa/pipe/tok/CLI.java
@@ -23,6 +23,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.impl.Arguments;
@@ -33,7 +34,7 @@ import net.sourceforge.argparse4j.inf.Namespace;
 /**
  * ixa-pipe tokenization
  * 
- *  
+ * 
  * @author ragerri
  * @version 1.0
  * 
@@ -52,7 +53,7 @@ public class CLI {
    */
 
   public static void main(String[] args) throws IOException {
-    
+
     Namespace parsedArguments = null;
 
     // create Argument Parser
@@ -68,27 +69,38 @@ public class CLI {
         .required(true)
         .help(
             "It is REQUIRED to choose a language to perform annotation with ixa-pipe-tok.\n");
-    
-    parser.addArgument("-n","--normalize").choices("ancora", "en", "ptb3", "sptb3")
-          .required(false).setDefault("en").help("Set normalization method; (s)ptb3 and ancora comply with " +
-          		"Penn Treebank and Ancora normalizations respectively; the default option does not escape " +
-          		"brackets or forward slashes. See README for more details.\n");
-    
-    parser.addArgument("--nokaf").action(Arguments.storeFalse())
-          .help("Do not print tokens in KAF format, but plain text.\n");
-    
-    parser.addArgument("-o", "--outputFormat").choices("conll", "oneline")
-            .setDefault("oneline")
-            .required(false)
-            .help("Choose between conll format (one token per line) or running tokenized text.\n");
 
-    // input tokenized and segmented text 
+    parser
+        .addArgument("-n", "--normalize")
+        .choices("ancora", "en", "ptb3", "sptb3")
+        .required(false)
+        .setDefault("en")
+        .help(
+            "Set normalization method; (s)ptb3 and ancora comply with "
+                + "Penn Treebank and Ancora normalizations respectively; the default option does not escape "
+                + "brackets or forward slashes. See README for more details.\n");
+
+    parser.addArgument("--nokaf").action(Arguments.storeFalse())
+        .help("Do not print tokens in KAF format, but plain text.\n");
+
+    parser
+        .addArgument("-o", "--outputFormat")
+        .choices("conll", "oneline")
+        .setDefault("oneline")
+        .required(false)
+        .help(
+            "Choose between conll format (one token per line) or running tokenized text.\n");
+
+    // input tokenized and segmented text
     parser.addArgument("--notok").action(Arguments.storeTrue())
         .help("Build a KAF document with already tokenized text.\n");
-    
+
     // specify whether input if a KAF/NAF file
-    parser.addArgument("-k", "--kaf").action(Arguments.storeTrue())
-        .help("Use this option if input is a KAF/NAF document with <raw> layer.\n");
+    parser
+        .addArgument("-k", "--kaf")
+        .action(Arguments.storeTrue())
+        .help(
+            "Use this option if input is a KAF/NAF document with <raw> layer.\n");
 
     // specify KAF version
     parser.addArgument("--kafversion").setDefault("v1.opener")
@@ -121,46 +133,45 @@ public class CLI {
     BufferedWriter bwriter = null;
     TokenFactory tokenFactory = new TokenFactory();
     KAFDocument kaf;
-  
+
     // reading standard input, segment and tokenize
     try {
-      breader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-      bwriter = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
-      String text;
       
+      bwriter = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"));
 
-      //TODO if this option is used, get language from lang attribute in KAF 
+      // TODO if this option is used, get language from lang attribute in KAF
       if (inputKafRaw) {
+        BufferedReader kafReader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
         // read KAF from standard input
-        //kaf = KAFDocument.createFromStream(breader);
-        //text = kaf.getRawText();
-      } 
-      else {
-        // write kaf
-        if (parsedArguments.getBoolean("nokaf")) {
-        kaf = new KAFDocument(lang, "v1.opener");
+        kaf = KAFDocument.createFromStream(kafReader);
+        String text = kaf.getRawText();
+        StringReader stringReader = new StringReader(text);
+        breader = new BufferedReader(stringReader);
+      } else {// take plain text and create a new KAFDocument
+        kaf = new KAFDocument(lang, kafVersion);
+        breader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+      }
+      // tokenize in kaf
+      if (parsedArguments.getBoolean("nokaf")) {
         kaf.addLinguisticProcessor("text", "ixa-pipe-tok-" + lang, "1.0");
-        
-          if (parsedArguments.getBoolean("notok")) {
-            Annotate annotator = new Annotate(breader);
-            bwriter.write(annotator.tokenizedTextToKAF(breader,kaf));
-          }
-          else {
-            Annotate annotator = new Annotate(breader,tokenFactory,normalize);
-            bwriter.write(annotator.tokensToKAF(kaf)); 
-          }
-        }// kaf options end here
-        
-        else {
-          Annotate annotator = new Annotate(breader,tokenFactory,normalize);
-          if (outputFormat.equalsIgnoreCase("conll")) { 
-            bwriter.write(annotator.tokensToCoNLL());
-          }
-          else { 
-            bwriter.write(annotator.tokensToText());
-          }
+        if (parsedArguments.getBoolean("notok")) {
+          Annotate annotator = new Annotate(breader);
+          bwriter.write(annotator.tokenizedTextToKAF(breader, kaf));
+        } else {
+          Annotate annotator = new Annotate(breader, tokenFactory, normalize);
+          bwriter.write(annotator.tokensToKAF(kaf));
+        }
+      }// kaf options end here
+
+      else {
+        Annotate annotator = new Annotate(breader, tokenFactory, normalize);
+        if (outputFormat.equalsIgnoreCase("conll")) {
+          bwriter.write(annotator.tokensToCoNLL());
+        } else {
+          bwriter.write(annotator.tokensToText());
         }
       }// annotation options end here
+      
       bwriter.close();
       breader.close();
     } catch (IOException e) {
